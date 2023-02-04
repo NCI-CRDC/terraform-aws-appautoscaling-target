ca82c7d902e756bea55bbd49ca193156366328e3
Cole DeVries <colemandevries@gmail.com>
2023-02-04 15:16:08 -0500

    initial commit

---
+++ main.tf
@@ -0,0 +1,7 @@
+resource "aws_appautoscaling_target" "microservice_autoscaling_target" {
+  max_capacity       = 5
+  min_capacity       = 1
+  resource_id        = "service/${aws_ecs_cluster.ecs_cluster.name}/${aws_ecs_service.service[each.key].name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
