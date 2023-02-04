ba8dd39551debba3b5d320ed8393f29d4cccc978
Cole DeVries <colemandevries@gmail.com>
2023-02-04 15:46:43 -0500

    updated outputs

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "id" {
 output "resource_id" {
   value = aws_appautoscaling_target.this.resource_id
 }
+
+output "scalable_dimension" {
+  value = aws_appautoscaling_target.this.scalable_dimension
+}
+
+output "service_namespace" {
+  value = aws_appautoscaling_target.this.service_namespace
+}
